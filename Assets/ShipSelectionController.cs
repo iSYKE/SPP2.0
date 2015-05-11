ac1ee946f2fe08b03de62c5da6b0506f623fa97d
ShadowWillow <dan72b@gmail.com>
2015-05-10 20:52:13 -0400

    Ship Selection Menu

---
+++ Assets/ShipSelectionController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSelectionController : MonoBehaviour {
+
+	ShipMovingOffMenu shipMovingOffMenu;
+	
+	public void ShipButtons(string shipName) {
+		if (shipName == "RedSloop") {
+			MoveShipOff();
+			StartCoroutine(KillShipCoroutine());
+		}
+		if (shipName == "BlueSloop") {
+
+		}
+		if (shipName == "YellowSloop") {
+
+		}
+	}
+	
+	void MoveShipOff() {
+		shipMovingOffMenu = GetComponent<ShipMovingOffMenu> ();
+		shipMovingOffMenu.enabled = true;
+	}
+
+	IEnumerator KillShipCoroutine(){
+		yield return new WaitForSeconds (10f);
+		shipMovingOffMenu.enabled = false;
+		Debug.Log ("This HAs Been 10Seconds");
+	}
+}

fceaff21c6357fa8da3f620d9c8d183d439ca987
ShadowWillow <dan72b@gmail.com>
2015-05-12 18:41:59 -0400

    Brig Selection Added To Main Menu

---
+++ Assets/LevelLoad.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoad : MonoBehaviour {
+
+	GameObject PlayerShip;
+
+	MenuSelectedShip menuSelectedShip;
+
+	void Start () {
+		menuSelectedShip = GetComponent<MenuSelectedShip>();
+
+
+	}
+
+	void Update () {
+
+	}
+}
